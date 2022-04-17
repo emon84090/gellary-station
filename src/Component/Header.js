42ab3a931d2775db9150a9f81fcd23a9eb18002c
emon84090 <emon84090@gmail.com>
2022-04-17 12:51:30 +0600

    header responsive done

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,48 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
+    const [menu, setMenu] = useState(false);
     return (
         <>
-            <header className='bg-white w-full py-4 sticky top-0'>
+            <header className='bg-white w-full py-4 sticky top-0 z-10 px-2'>
                 <div className="container mx-auto">
                     <div className="header-all-content flex-wrap flex justify-between items-center ">
                         <div className="logo">
                             <Link className='text-2xl font-semibold' to="/">Gellary <span className='text-yellow-500'>Station</span></Link>
                         </div>
-                        <div className="header-menu">
-                            <ul>
-                                <li><NavLink
-                                    className={({ isActive }) => (`font-semibold text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
-                                    to='/home'
-                                >
-                                    Home
-                                </NavLink></li>
-
-                                <li><NavLink
-                                    className={({ isActive }) => (`font-semibold text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
-                                    to='/ser'
-                                >
-                                    services
-                                </NavLink></li>
-
-                                <li><NavLink
-                                    className={({ isActive }) => (`font-semibold text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
-                                    to='/contact'
-                                >
-                                    Contact
-                                </NavLink></li>
-
-                            </ul>
-
+                        <div className="menu md:hidden mx-3">
+                            <i onClick={() => setMenu(!menu)} className='bx bx-menu text-2xl cursor-pointer'></i>
                         </div>
+                        <div className={`header-utilities  md:flex items-center text-center mt-3 md:mt-0 ${!menu ? 'max-h-0 md:max-h-max overflow-hidden' : 'max-h-max'}`}>
+                            <div className="header-menu md:mr-5">
+                                <ul>
+                                    <li><NavLink
+                                        className={({ isActive }) => (`font-semibold text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
+                                        to='/home'
+                                    >
+                                        Home
+                                    </NavLink></li>
 
-                        <div className="header-link flex">
-                            <div className="signup-btn ">
-                                <button className='bg-gray-900 w-20 h-10 text-white rounded-md'><Link className='h-full font-semibold justify-center flex items-center' to="/registration">Sign Up</Link></button>
+                                    <li><a className='font-semibold text-md capitalize' href="home#services">Services</a></li>
+
+                                </ul>
+
                             </div>
-                            <div className="login-btn ml-3">
-                                <button className='bg-gray-900 w-20 h-10 text-white rounded-md'><Link className='h-full font-semibold justify-center flex items-center' to="/login">Login</Link></button>
+
+                            <div className="header-link md:flex">
+                                <div className="signup-btn mt-3 md:mt-0 flex items-center justify-center">
+                                    <NavLink
+                                        className={({ isActive }) => (`font-semibold  text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
+                                        to='/registration'
+                                    >
+                                        SignUp
+                                    </NavLink>
+                                </div>
+                                <div className="login-btn ml-3 mt-3 md:mt-0">
+                                    <button className='bg-gray-900 w-20 h-10 text-white rounded-md'><NavLink
+                                        className={({ isActive }) => (`font-semibold text-md capitalize ${isActive ? "text-yellow-500" : ""}`)}
+                                        to='/login'
+                                    >
+                                        Login
+                                    </NavLink></button>
+                                </div>
                             </div>
                         </div>
                     </div>
--- src/Component/Home page/Banner.js
+++ src/Component/Home page/Banner.js
@@ -3,17 +3,17 @@ import React from 'react';
 const Banner = () => {
     return (
         <>
-            <div className="banner-all-content flex items-center">
-                <div className="container mx-auto h-full">
+            <div className="banner-all-content flex items-center py-16">
+                <div className="container mx-auto h-full px-1 ">
                     <div className="banner-content grid grid-cols-12 place-content-center h-full">
-                        <div className="banner-left col-span-5" data-aos="fade-up">
-                            <h2 className='text-4xl font-bold capitalize '>Hello i am Arif Khan , i am professional photographer</h2>
+                        <div className="banner-left col-span-12 md:col-span-5 order-2 mt-4">
+                            <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold capitalize '>Hello i am Arif Khan , i am professional photographer</h2>
 
                             <p className='mt-5 text-gray-700 bg-opacity-70 text-md capitalize font-light'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis dolore culpa quas, iste, maxime laudantium ex aliquid </p>
 
                             <button className='w-40 h-12 bg-gray-900 mt-10 rounded-sm text-white font-semibold shadow-sm'>Contact Me</button>
                         </div>
-                        <div className="banner-right col-span-7">
+                        <div className="banner-right col-span-12 md:col-span-7  order-1 md:order-3">
                             <img src="https://i.ibb.co/nszqbRp/young-active-male-traveler-takes-photo-with-retro-camera-dressed-hat-raincoat-as-travels-during-rain.png" alt="" />
                         </div>
                     </div>
